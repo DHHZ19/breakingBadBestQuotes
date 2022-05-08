3762ed6df73aaa0ee66740429077330fac1f86f2
Diego Hernandez <diegofootball19@gmail.com>
2022-05-08 13:04:40 -0500

    changes

--- breakingBad.js
+++ breakingBad.js
@@ -1,23 +1,31 @@
 //Example fetch using pokemonapi.co
  document.querySelector('button').addEventListener('click', getFetch)
 
-
-
+ const select = document.querySelector("select")
+//  author: "Saul Goodman"
 function getFetch(){
   const url = `https://breaking-bad-quotes.herokuapp.com/v1/quotes/10`
-  const choice = document.querySelector('#input').value.toLowerCase()
+  // const choice = document.querySelector('#input').value.toLowerCase()
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        console.log(choice)
-        for(let i = 0; i < data.length; i++){
-            if(data[i].author.toLowerCase() == `${choice}`){
-            document.querySelector('.author').innerText = data[i].author
-            document.querySelector('p').innerText = data[i].quote
-        }
-    }
+        let characters = []
+        
+       data.forEach((x,i) => characters.push(data[i].author))
+       for(let i = 0; i < data.length; i++){
+      document.querySelector('select').appendChild(document.createElement('option')).text = characters.sort()[i]
+      select.addEventListener('change', event => {
+        document.querySelector('.author').innerText = data[i].quote
+        document.querySelector('p').innerText = data[i].author
+  
+      })
+       }
+            //event listener for choice
+     
+
+     
       })
       
       .catch(err => {
